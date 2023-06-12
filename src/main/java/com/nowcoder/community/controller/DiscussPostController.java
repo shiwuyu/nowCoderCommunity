a80c4689a048eec2a73423724fe9ff07af98dbb1
shiwuyu <shiwuyu9527@foxmail.com>
2023-06-12 20:48:54 +0800

    3.2 AJAX异步，发布帖子

---
+++ src/main/java/com/nowcoder/community/controller/DiscussPostController.java
@@ -0,0 +1,51 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.DiscussPostService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * @Program: community
+ * @Description: 评论控制器
+ * @Author: 张非凡
+ * @Create: 2023-06-11 16:55
+ */
+
+@Controller
+@RequestMapping("/discuss")
+public class DiscussPostController {
+
+    @Autowired
+    private DiscussPostService discussPostService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public String addDiscussPost(String title, String content) {
+        User user = hostHolder.getUser();
+        if (user == null) {
+            return CommunityUtil.getJSONString(403, "你还未登录!请登录");
+        }
+
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle(title);
+        post.setContent(content);
+        post.setCreateTime(new Date());
+        discussPostService.addDiscussPosts(post);
+
+        return CommunityUtil.getJSONString(0, "发布成功!");
+    }
+
+}
--- src/main/java/com/nowcoder/community/service/DiscussPostService.java
+++ src/main/java/com/nowcoder/community/service/DiscussPostService.java
@@ -3,8 +3,10 @@ package com.nowcoder.community.service;
 import com.fasterxml.jackson.databind.annotation.JsonAppend;
 import com.nowcoder.community.dao.DiscussPostMapper;
 import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.util.SensitiveFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.util.HtmlUtils;
 
 import java.util.List;
 
@@ -20,11 +22,29 @@ public class DiscussPostService {
     @Autowired
     private DiscussPostMapper discussPostMapper;
 
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
     public List<DiscussPost> findDiscussPosts(int userId, int offset, int limit) {
         return discussPostMapper.selectDiscussPosts(userId, offset, limit);
     }
 
     public int findDiscussPostRows(int userId) {
         return discussPostMapper.selectDiscussPostRows(userId);
+    }
+
+    public int addDiscussPosts(DiscussPost post) {
+        if (post == null) {
+            throw new IllegalArgumentException("参数不能为空!");
+        }
+
+        // 转义HTML不能为空!
+        post.setTitle(HtmlUtils.htmlEscape(post.getTitle()));
+        post.setContent(HtmlUtils.htmlEscape(post.getContent()));
+        // 过滤敏感词
+        post.setTitle(sensitiveFilter.filter(post.getTitle()));
+        post.setContent(sensitiveFilter.filter(post.getContent()));
+
+        return discussPostMapper.insertDiscussPost(post);
     }
 }
