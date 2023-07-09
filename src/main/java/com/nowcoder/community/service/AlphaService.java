59026d609f3de98487e74378695c42717215095a
shiwuyu <shiwuyu9527@foxmail.com>
2023-07-09 23:46:20 +0800

    3.4 SpringBoot事务的练习

--- src/main/java/com/nowcoder/community/service/AlphaService.java
+++ src/main/java/com/nowcoder/community/service/AlphaService.java
@@ -1,10 +1,25 @@
 package com.nowcoder.community.service;
 
+import com.nowcoder.community.dao.DiscussPostMapper;
+import com.nowcoder.community.dao.UserMapper;
+import com.nowcoder.community.entity.DiscussPost;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.util.CommunityUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionCallback;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import java.beans.Transient;
+import java.util.Date;
 
 /**
  * @program: community
@@ -15,6 +30,15 @@ import javax.annotation.PreDestroy;
 @Service
 @Scope("prototype")
 public class AlphaService {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Autowired
+    private DiscussPostMapper discussPostMapper;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
 
     public AlphaService() {
         System.out.println("实例化AlphaService");
@@ -28,6 +52,70 @@ public class AlphaService {
     @PreDestroy
     public void destroy() {
         System.out.println("销毁AlphaService");
+    }
+
+    /**
+     *   REQUIRED: 支持当前事务(外部事务),如果不存在则创建新事务.
+     *   REQUIRES_NEW: 创建一个新事务,并且暂停当前事务(外部事务).
+     *   NESTED: 如果当前存在事务(外部事务),则嵌套在该事务中执行(独立的提交和回滚),否则就会REQUIRED一样.
+     *   save1()是声明式事务,它只加了一个注解,没有入侵代码
+     */
+    @Transactional(isolation = Isolation.READ_COMMITTED, propagation = Propagation.REQUIRED)
+    public Object save1() {
+        //新增用户
+        User user = new User();
+        user.setUsername("alpha");
+        user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+        user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+        user.setEmail("alpha@qq.com");
+        user.setHeaderUrl("http://image.nowcoder.com/head/99t.png");
+        user.setCreateTime(new Date());
+        userMapper.insertUser(user);
+
+        // 新建帖子
+        DiscussPost post = new DiscussPost();
+        post.setUserId(user.getId());
+        post.setTitle("Hello");
+        post.setContent("新人报道!");
+        post.setCreateTime(new Date());
+        discussPostMapper.insertDiscussPost(post);
+
+        // 这是干嘛?
+        //Integer.valueOf("abc");
+
+        return "OK";
+    }
+
+    public Object save2() {
+        transactionTemplate.setIsolationLevel(TransactionDefinition.ISOLATION_READ_COMMITTED);
+        transactionTemplate.setPropagationBehavior(TransactionTemplate.PROPAGATION_REQUIRED);
+
+        return transactionTemplate.execute(new TransactionCallback<Object>() {
+            @Override
+            public Object doInTransaction(TransactionStatus status) {
+                // 新增用户
+                User user = new User();
+                user.setUsername("beta");
+                user.setSalt(CommunityUtil.generateUUID().substring(0, 5));
+                user.setPassword(CommunityUtil.md5("123" + user.getSalt()));
+                user.setEmail("beta@qq.com");
+                user.setHeaderUrl("http://image.nowcoder.com/head/999t.png");
+                user.setCreateTime(new Date());
+                userMapper.insertUser(user);
+
+                // 新增帖子
+                DiscussPost post = new DiscussPost();
+                post.setUserId(user.getId());
+                post.setTitle("你好");
+                post.setContent("我是新人!");
+                post.setCreateTime(new Date());
+                discussPostMapper.insertDiscussPost(post);
+
+                //Integer.valueOf("abc");
+
+                return "ok";
+            }
+        });
     }
 
 }
