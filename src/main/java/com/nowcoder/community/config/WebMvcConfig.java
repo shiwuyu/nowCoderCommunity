5b6c724ce29348dae2cda8a2080f881a8338a805
shiwuyu <shiwuyu9527@foxmail.com>
2023-11-05 09:54:00 +0800

    5.13 发送系统通知&显示系统通知

--- src/main/java/com/nowcoder/community/config/WebMvcConfig.java
+++ src/main/java/com/nowcoder/community/config/WebMvcConfig.java
@@ -1,10 +1,9 @@
 package com.nowcoder.community.config;
 
-import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.controller.Interceptor.AlphaInterceptor;
 import com.nowcoder.community.controller.Interceptor.LoginRequiredInterceptor;
 import com.nowcoder.community.controller.Interceptor.LoginTicketInterceptor;
-import org.slf4j.LoggerFactory;
+import com.nowcoder.community.controller.Interceptor.MessageInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -31,6 +30,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Autowired
     private LoginRequiredInterceptor loginRequiredInterceptor;
 
+    @Autowired
+    private MessageInterceptor messageInterceptor;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(alphaInterceptor)
@@ -40,5 +42,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.jepg");
         registry.addInterceptor(loginRequiredInterceptor)
                 .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.jpg", "/**/*.jepg");
+        registry.addInterceptor(messageInterceptor)
+                .excludePathPatterns("/**/*.css", "/**/*.js", "/**/*.png", "/**/*.jpg", "/**/*.jpeg");
     }
 }
--- src/main/java/com/nowcoder/community/dao/MessageMapper.java
+++ src/main/java/com/nowcoder/community/dao/MessageMapper.java
@@ -16,51 +16,90 @@ import java.util.List;
 public interface MessageMapper {
 
     /**
-     * @description: 查询当前用户会话列表，针对每个会话值返回一条最新私信
+     * 查询当前用户会话列表，针对每个会话值返回一条最新私信
      * @param: [userId, offset, limit]
      * @return: java.util.List<com.nowcoder.community.entity.Message>
      */
     List<Message> selectConversations(int userId, int offset, int limit);
 
     /**
-     * @description: 查询当前用户会话数量
+     * 查询当前用户会话数量
      * @param: [userId]
      * @return: int
      */
     int selectConversationCount(int userId);
 
     /**
-     * @description: 查询某个会话包含的私信列表
+     * 查询某个会话包含的私信列表
      * @param: [conversationId, offset, limit]
      * @return: java.util.List<com.nowcoder.community.entity.Message>
      */
     List<Message> selectLetters(String conversationId, int offset, int limit);
 
     /**
-     * @description: 查询某个会话包含的私信数量
+     * 查询某个会话包含的私信数量
      * @param: [conversationId]  会话ID
      * @return: int
      */
     int selectLetterCount(String conversationId);
 
     /**
-     * @description: 查询未读私信的数量
+     * 查询未读私信的数量
      * @param: [userId, conversationId]
      * @return: int
      */
     int selectLetterUnreadCount(int userId, String conversationId);
 
     /**
-     * @description: 新增消息
+     * 新增消息
      * @param: [message]
      * @return: int
      */
     int insertMessage(Message message);
 
     /**
-     * @description: 修改消息状态
-     * @param: [ids, status]
+     * 修改消息状态
+     * @param ids
+     * @param status
      * @return: int
      */
     int updateStatus(List<Integer> ids, int status);
+
+    /**
+     * 查询某个主题下最新的通知
+     *
+     * @param userId
+     * @param topic
+     * @return com.nowcoder.community.entity.Message
+     */
+    Message selectLatestNotice (int userId, String topic);
+
+    /**
+     * 查询某个主题锁包含的通知数量
+     *
+     * @param userId
+     * @param topic
+     * @return int
+     */
+    int selectNoticeCount(int userId, String topic);
+
+    /**
+     * 查询未读的通知的数量
+     *
+     * @param userId
+     * @param topic
+     * @return int
+     */
+    int selectNoticeUnreadCount(int userId, String topic);
+
+    /**
+     * 查询某个主题包含的通知列表
+     *
+     * @param userId
+     * @param topic
+     * @param offset
+     * @param limit
+     * @return java.util.List<com.nowcoder.community.entity.Message>
+     */
+    List<Message> selectNotices(int userId, String topic, int offset, int limit);
 }
